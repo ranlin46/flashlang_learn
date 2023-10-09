cc19c6486a46e26c086d7609d09f29e02509db14
ranlin <ranlin0406@foxmail.com>
2023-10-09 15:49:00 +0800

    添加条件，让学习状态能够转变为3

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from datetime import datetime, timedelta
 pygame.init()
 
 # 连接到数据库文件
-conn = sqlite3.connect('flashcards_database.db')  # 表1
+conn = sqlite3.connect('flashcards_database.db')
 cursor = conn.cursor()
+learning_table_name = "Table2"
 
-learning_table_name = "Table2"  # 学习历史表的表名
 
-
+# 定义Flashcard类
 class Flashcard:
     def __init__(self, id, status, phonetic, word, sentence, audio_url, learn_date, next_review_date):
         self.id = id
@@ -25,8 +25,14 @@ class Flashcard:
         self.next_review_date = next_review_date
 
 
+# 初始化数据库表格
 def initialize_tables():
-    # 检查并创建表2
+    create_learning_history_table()
+    insert_initial_data()
+
+
+# 创建学习历史表格
+def create_learning_history_table():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Table2 (
             RecordID INTEGER PRIMARY KEY,
@@ -38,82 +44,70 @@ def initialize_tables():
         )
     ''')
 
-    inserted_card_ids = set()  # 用于跟踪已经插入的卡片的集合
 
-    # 插入初始数据，确保表2中有10条不重复的数据
+# 插入初始数据
+def insert_initial_data():
+    inserted_card_ids = set()
     while not enough_data_in_table2():
         flashcard = random.choice(load_flashcards())
-
-        # 检查是否已经插入过该卡片，如果是，则重新选择一个
         while flashcard.id in inserted_card_ids:
             flashcard = random.choice(load_flashcards())
-
-        # 插入数据
         cursor.execute('''
             INSERT INTO Table2 (LearnDate, CardID, YesCount, NoCount, LearningStatus)
             VALUES (?, ?, ?, ?, ?)
         ''', (0, flashcard.id, 0, 0, flashcard.status))
-
-        inserted_card_ids.add(flashcard.id)  # 将插入的卡片ID添加到集合中
+        inserted_card_ids.add(flashcard.id)
         conn.commit()
 
-    conn.commit()
 
-
+# 检查是否有足够的数据在表2中
 def enough_data_in_table2():
     cursor.execute("SELECT COUNT(*) FROM Table2")
     count = cursor.fetchone()[0]
     return count >= 10
 
 
+# 加载单词卡片
 def load_flashcards():
-    # 查询NextReviewDate为当天的卡片
     cursor.execute("SELECT * FROM flashcards_database WHERE status IN (1, 2) AND NextReviewDate = date('now')")
     records = cursor.fetchall()
-
     if records:
         flashcards = [Flashcard(*record) for record in records]
     else:
-        # 如果没有NextReviewDate为当天的卡片，则查询其他卡片
         cursor.execute("SELECT * FROM flashcards_database WHERE status IN (1, 2)")
         records = cursor.fetchall()
         flashcards = [Flashcard(*record) for record in records]
-
     random.shuffle(flashcards)
     return flashcards
 
 
+# 获取待学习的单词卡片
 def get_flashcards(learning_table_name):
-    # 从表2中获取所有的 card_id
     cursor.execute(f"SELECT DISTINCT CardID FROM {learning_table_name}")
     card_ids = [row[0] for row in cursor.fetchall()]
-
     flashcards = []
-
-    # 根据表2中的 card_id 从表1中加载对应的内容
     for card_id in card_ids:
         cursor.execute("SELECT * FROM flashcards_database WHERE id = ? ", (card_id,))
         record = cursor.fetchone()
         if record:
             flashcard = Flashcard(*record)
             flashcards.append(flashcard)
-
     random.shuffle(flashcards)
     return flashcards
 
 
+# 展示单词卡片
 def show_flashcard(flashcard):
     print(f"Phonetic: {flashcard.phonetic}")
     print(f"Word: {flashcard.word}")
     print(f"Sentence: {flashcard.sentence}")
-
-    # 播放音频
-    pygame.mixer.music.load(flashcard.audio_url)  # 使用音频文件的路径
+    pygame.mixer.music.load(flashcard.audio_url)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
-        pygame.time.delay(100)  # 每100毫秒检查一次音乐是否仍在播放
+        pygame.time.delay(100)
 
 
+# 更新学习状态
 def update_status(choice, learning_table_name, flashcard):
     if choice == "yes":
         cursor.execute(f"UPDATE {learning_table_name} SET YesCount = YesCount + 1 WHERE CardID = ?",
@@ -123,44 +117,32 @@ def update_status(choice, learning_table_name, flashcard):
                        (flashcard.id,))
     else:
         print("无效的选项")
-
-    # 更新学习历史记录中的数据
     cursor.execute(
         f"UPDATE {learning_table_name} SET LearnDate = date('now'), LearningStatus = ? WHERE CardID = ?",
         (decide_learning_status(learning_table_name, flashcard), flashcard.id))
-
-    # 提交更改到数据库
     conn.commit()
 
 
+# 决定学习状态
 def decide_learning_status(learning_table_name, flashcard):
-    # 查询学习历史表格中的 "YesCount" 和 "NoCount" 数量
     cursor.execute(f"SELECT YesCount, NoCount FROM {learning_table_name} WHERE CardID = ?", (flashcard.id,))
     result = cursor.fetchone()
     yes_count = result[0] if result else 0
     no_count = result[1] if result else 0
-
-    # 根据 "YesCount" 和 "NoCount" 的数量关系选择学习状态
     if yes_count - no_count > 0:
         return 2
     else:
         return 1
 
 
+# 创建个体学习记录表格
 def create_individual_learning_tables(learning_table_name):
-    # 查询表2中不同的CardID
     cursor.execute(f"SELECT DISTINCT CardID FROM {learning_table_name}")
     card_ids = [row[0] for row in cursor.fetchall()]
-
-    # 为每个CardID创建一个独立的学习记录表格
     for card_id in card_ids:
-        # 查询表2中的学习记录
         cursor.execute(f"SELECT * FROM {learning_table_name} WHERE CardID = ?", (card_id,))
         record = cursor.fetchone()
-
-        # 判断是否应该创建表3
         if record[1] != 0:
-            # 表3的名称格式：individual_record_CardID
             table_name = f"individual_record_{card_id}"
             cursor.execute(f'''
                 CREATE TABLE IF NOT EXISTS {table_name} (
@@ -173,80 +155,61 @@ def create_individual_learning_tables(learning_table_name):
                     NextReviewDate DATE
                 )
             ''')
-
     conn.commit()
 
 
+# 记录学习状态到个体学习记录表格
 def record_learning_status_for_individual_tables(learning_table_name):
-    # 查询表2中的学习记录
     cursor.execute(f"SELECT * FROM {learning_table_name}")
     records = cursor.fetchall()
-
     for record in records:
-        card_id = record[2]  # 获取CardID
-        # 表3的名称格式：individual_record_CardID
+        card_id = record[2]
         table_name = f"individual_record_{card_id}"
-
-        # 判断是否应该插入学习记录到表3
         if record[1] != 0:
-            # 插入学习记录到相应的表3中
             cursor.execute(f'''
                 INSERT INTO {table_name} (CardID, LearnDate, YesCount, NoCount, LearningStatus)
                 VALUES (?, ?, ?, ?, ?)
             ''', (record[2], record[1], record[3], record[4], record[5]))
             conn.commit()
-            # 在这里添加判断，只有当表3中只有一行内容时才执行下一个函数
             cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
             count = cursor.fetchone()[0]
-
             if count == 1:
-                # 表3中只有一行内容，执行下一个函数
                 insert_initial_review_dates(card_id)
                 update_next_review_date(card_id)
             else:
                 update_next_review_date(card_id)
-
     conn.commit()
 
 
+# 插入初始复习日期
 def insert_initial_review_dates(card_id):
     review_list = [1, 2, 3, 4, 7, 8, 15, 30]
     table_name = f"individual_record_{card_id}"
-
-    # 查询表3中是否已存在相关数据
     cursor.execute(f"SELECT LearnDate FROM {table_name}")
-    learn_date_str = cursor.fetchone()[0]  # 获取学习日期字符串
-
-    # 将学习日期字符串解析为日期对象
+    learn_date_str = cursor.fetchone()[0]
     current_study_date = datetime.strptime(learn_date_str, '%Y-%m-%d').date()
-
-    # 依次插入多个复习日期
     for interval in review_list:
-        # 计算下次复习日期
         next_review_date = current_study_date + timedelta(days=interval)
-
-        # 插入下次复习日期到表3中
         cursor.execute(f"INSERT INTO {table_name} (CardID, NextReviewDate) VALUES (?, ?)", (card_id, next_review_date))
-
     conn.commit()
 
 
+# 更新下次复习日期
 def update_next_review_date(card_id):
     table_name = f"individual_record_{card_id}"
-
-    # 查询LearnDate为空的第一条NextReviewDate数据
     cursor.execute(f"SELECT NextReviewDate FROM {table_name} WHERE CardID = ? AND LearnDate IS NULL ORDER BY "
                    f"NextReviewDate ASC LIMIT 1", (card_id,))
     result = cursor.fetchone()
-
     if result:
         next_review_date = result[0]
-
-        # 更新表1中的NextReviewDate字段
         cursor.execute("UPDATE flashcards_database SET NextReviewDate = ? WHERE id = ?", (next_review_date, card_id))
+        conn.commit()
+    else:
+        cursor.execute("UPDATE flashcards_database SET status = 3 WHERE id = ?", (card_id,))
         conn.commit()
 
 
+# 更新单词卡片状态
 def update_flashcard_status(choice, flashcard):
     if choice == 'yes' or choice == 'no':
         update_status(choice, learning_table_name, flashcard)
@@ -254,47 +217,37 @@ def update_flashcard_status(choice, flashcard):
         print("无效的选项")
 
 
+# 清理数据并退出程序
 def cleanup_and_exit(learning_table_name, flashcards):
     for flashcard in flashcards:
-        # 计算学习状态
         status = decide_learning_status(learning_table_name, flashcard)
-
-        # 更新表1的状态和日期
         cursor.execute(f"""
             UPDATE flashcards_database 
             SET status = ?, LearnDate = (SELECT LearnDate FROM {learning_table_name} WHERE CardID = ?)
             WHERE id = ?
         """, (status, flashcard.id, flashcard.id))
         conn.commit()
-
-        conn.commit()
-
     create_individual_learning_tables(learning_table_name)
     record_learning_status_for_individual_tables(learning_table_name)
-
-    # 使用DELETE语句删除学习状态为2的记录
     cursor.execute(f"DELETE FROM {learning_table_name} WHERE LearningStatus = 2 AND (YesCount > 0 OR NoCount > 0)")
     conn.commit()
-
     conn.close()
     print("学习完成！")
 
 
+# 主函数
 def main():
-    initialize_tables()  # 初始化表2
+    initialize_tables()
     flashcards = get_flashcards(learning_table_name)
-    quit_program = False  # 添加一个标志来控制是否退出程序
-
+    quit_program = False
     while flashcards and not quit_program:
-        flashcard = random.choice(flashcards)  # 随机选择一个卡片
+        flashcard = random.choice(flashcards)
         show_flashcard(flashcard)
         choice = input("\nChoose an action (yes/no) or press 'q' to quit: ").lower()
-
         if choice == 'q':
             quit_program = True
         else:
             update_flashcard_status(choice, flashcard)
-
     cleanup_and_exit(learning_table_name, flashcards)
 
 
