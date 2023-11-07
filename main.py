72be55fa80623790ee32af2bd086538e500ed8ae
ranlin <ranlin0406@foxmail.com>
2023-11-07 16:14:56 +0800

    数据库版11.07

--- main.py
+++ main.py
@@ -8,109 +8,26 @@ pygame.init()
 conn = sqlite3.connect('flashcards_database.db')
 cursor = conn.cursor()
 
-LEARNING_TABLE_NAME = "Table2"
-
 
 class Flashcard:
-    def __init__(self, id, status, phonetic, word, sentence, audio_url, learn_date, next_review_date):
-        self.id = id
-        self.status = status
+    def __init__(self, card_id, phonetic, word, sentence, audio_url):
+        self.id = card_id
         self.phonetic = phonetic
         self.word = word
         self.sentence = sentence
         self.audio_url = audio_url
-        self.learn_date = learn_date
-        self.next_review_date = next_review_date
-
-
-def initialize_tables():
-    create_learning_history_table()
-    insert_initial_data()
-
-
-def create_learning_history_table():
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Table2 (
-            RecordID INTEGER PRIMARY KEY,
-            LearnDate DATE,
-            CardID INTEGER,
-            YesCount INTEGER,
-            NoCount INTEGER,
-            LearningStatus INTEGER
-        )
-    ''')
-
-
-def execute_query(query, params=()):
-    cursor.execute(query, params)
-    conn.commit()
-
-
-def fetch_all(query, params=()):
-    cursor.execute(query, params)
-    return cursor.fetchall()
-
-
-def load_flashcards():
-    today = datetime.now().date()
-    query = "SELECT * FROM flashcards_database WHERE status IN (1, 2) AND NextReviewDate = ?"
-    records = fetch_all(query, (today,))
-
-    query = "SELECT DISTINCT CardID FROM Table2"
-    card_ids_in_table2 = [row[0] for row in fetch_all(query)]
-
-    filtered_records = []
-    for record in records:
-        card_id = record[0]
-        if card_id not in card_ids_in_table2:
-            filtered_records.append(record)
-
-    if not filtered_records:
-        query = "SELECT * FROM flashcards_database WHERE status IN (1, 2)"
-        records = fetch_all(query)
-        for record in records:
-            card_id = record[0]
-            if card_id not in card_ids_in_table2:
-                filtered_records.append(record)
-
-    flashcards = [Flashcard(*record) for record in filtered_records]
-    random.shuffle(flashcards)
-    return flashcards
-
-
-def enough_data_in_table2():
-    query = "SELECT COUNT(*) FROM Table2"
-    count = fetch_all(query)[0][0]
-    return count >= 10
-
-
-def insert_initial_data():
-    query = "SELECT DISTINCT CardID FROM Table2"
-    inserted_card_ids = [row[0] for row in fetch_all(query)]
-
-    while not enough_data_in_table2():
-        flashcard = random.choice(load_flashcards())
-        while flashcard.id in inserted_card_ids:
-            flashcard = random.choice(load_flashcards())
-
-        query = "INSERT INTO Table2 (LearnDate, CardID, YesCount, NoCount, LearningStatus) VALUES (?, ?, ?, ?, ?)"
-        execute_query(query, (0, flashcard.id, 0, 0, flashcard.status))
-        inserted_card_ids.append(flashcard.id)
 
 
 def get_flashcards():
-    query = f"SELECT DISTINCT CardID FROM {LEARNING_TABLE_NAME}"
-    card_ids = [row[0] for row in fetch_all(query)]
+    query = "SELECT * FROM Cards ORDER BY RANDOM() LIMIT 4"
+    cursor.execute(query)
+
     flashcards = []
+    for row in cursor.fetchall():
+        card_id, phonetic, word, sentence, audio_url = row
+        flashcard = Flashcard(card_id, phonetic, word, sentence, audio_url)
+        flashcards.append(flashcard)
 
-    for card_id in card_ids:
-        query = "SELECT * FROM flashcards_database WHERE id = ?"
-        record = fetch_all(query, (card_id,))
-        if record:
-            flashcard = Flashcard(*record[0])
-            flashcards.append(flashcard)
-
-    random.shuffle(flashcards)
     return flashcards
 
 
@@ -125,175 +42,136 @@ def show_flashcard(flashcard):
         pygame.time.delay(100)
 
 
-def record_count(choice, flashcard):
-    if choice == "yes":
-        query = f"UPDATE {LEARNING_TABLE_NAME} SET YesCount = YesCount + 1 WHERE CardID = ?"
-        execute_query(query, (flashcard.id,))
-    elif choice == "no":
-        query = f"UPDATE {LEARNING_TABLE_NAME} SET NoCount = NoCount + 1 WHERE CardID = ?"
-        execute_query(query, (flashcard.id,))
+def check_user_existence(username):
+    cursor.execute("SELECT COUNT(*) FROM Users WHERE Username = ?", (username,))
+    count = cursor.fetchone()[0]
+    return count > 0
+
+
+def insert_user(username, password, registration_date):
+    cursor.execute("INSERT INTO Users (Username, Password, RegistrationDate) VALUES (?, ?, ?)",
+                   (username, password, registration_date))
+    conn.commit()
+
+
+def insert_study_record(user_id, card_id, study_count, review_count):
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    # 检查是否已经有记录
+    cursor.execute("SELECT StudyCount, ReviewCount FROM StudyRecords WHERE UserID = ? AND CardID = ? AND StudyDate = ?",
+                   (user_id, card_id, current_date))
+    existing_record = cursor.fetchone()
+
+    if existing_record:
+        # 如果有现有记录，更新它
+        existing_study_count, existing_review_count = existing_record
+        study_count += existing_study_count
+        review_count += existing_review_count
+        cursor.execute(
+            "UPDATE StudyRecords SET StudyCount = ?, ReviewCount = ? WHERE UserID = ? AND CardID = ? AND StudyDate = ?",
+            (study_count, review_count, user_id, card_id, current_date))
     else:
-        print("Invalid choice")
+        # 如果没有现有记录，插入新记录
+        cursor.execute(
+            "INSERT INTO StudyRecords (UserID, CardID, StudyDate, StudyCount, ReviewCount) VALUES (?, ?, ?, ?, ?)",
+            (user_id, card_id, current_date, study_count, review_count))
 
-    status = decide_learning_status(flashcard)
-    query = f"UPDATE {LEARNING_TABLE_NAME} SET LearnDate = date('now'), LearningStatus = ? WHERE CardID = ?"
-    execute_query(query, (status, flashcard.id))
+    conn.commit()
 
 
-def decide_learning_status(flashcard):
-    query = f"SELECT YesCount, NoCount FROM {LEARNING_TABLE_NAME} WHERE CardID = ?"
-    result = fetch_all(query, (flashcard.id,))
-    yes_count, no_count = result[0] if result else (0, 0)
-
-    if yes_count - no_count > 0:
-        return 2
-    else:
+def calculate_study_status(study_count, review_count):
+    if study_count == 0 and review_count == 0:
         return 1
-
-
-def create_individual_learning_tables():
-    query = f"SELECT DISTINCT CardID FROM {LEARNING_TABLE_NAME}"
-    card_ids = [row[0] for row in fetch_all(query)]
-
-    for card_id in card_ids:
-        query = f"SELECT * FROM {LEARNING_TABLE_NAME} WHERE CardID = ?"
-        record = fetch_all(query, (card_id,))
-
-        if record[0][1] != 0:
-            table_name = f"individual_record_{card_id}"
-
-            cursor.execute(f'''
-                CREATE TABLE IF NOT EXISTS {table_name} (
-                    RecordID INTEGER PRIMARY KEY,
-                    CardID INTEGER,
-                    LearnDate DATE,
-                    YesCount INTEGER,
-                    NoCount INTEGER,
-                    LearningStatus INTEGER,
-                    NextReviewDate DATE
-                )
-            ''')
-
-
-def record_learning_status_for_individual_tables():
-    query = f"SELECT * FROM {LEARNING_TABLE_NAME}"
-    records = fetch_all(query)
-
-    for record in records:
-        card_id = record[2]
-        learn_date = record[1]
-        table_name = f"individual_record_{card_id}"
-        if learn_date != 0:
-            query = f"SELECT COUNT(*) FROM {table_name}"
-            count = fetch_all(query)[0][0]
-            if count == 0:
-                query = f'''
-                    INSERT INTO {table_name} (CardID, LearnDate, YesCount, NoCount, LearningStatus)
-                    VALUES (?, ?, ?, ?, ?)'''
-                execute_query(query, (card_id, learn_date, record[3], record[4], record[5]))
-
-                query = f"SELECT COUNT(*) FROM {table_name}"
-                count = fetch_all(query)[0][0]
-
-                if count == 1:
-                    insert_initial_review_dates(card_id)
-            else:
-                query = f'''
-                    UPDATE {table_name}
-                    SET YesCount = ?, NoCount = ?, LearningStatus = ?, LearnDate = ?
-                    WHERE NextReviewDate = ?'''
-                execute_query(query, (record[3], record[4], record[5], learn_date, learn_date))
-
-            update_next_review_date(card_id)
-
-
-def insert_initial_review_dates(card_id):
-    review_list = [1, 2, 3, 4, 7, 8, 15, 30]
-    table_name = f"individual_record_{card_id}"
-
-    query = f"SELECT LearnDate FROM {table_name}"
-    learn_date_str = fetch_all(query)[0][0]
-    current_study_date = datetime.strptime(learn_date_str, '%Y-%m-%d').date()
-
-    for interval in review_list:
-        next_review_date = current_study_date + timedelta(days=interval)
-        query = f"INSERT INTO {table_name} (CardID, NextReviewDate) VALUES (?, ?)"
-        execute_query(query, (card_id, next_review_date))
-
-
-def update_next_review_date(card_id):
-    table_name = f"individual_record_{card_id}"
-    query = f'''
-        SELECT NextReviewDate
-        FROM {table_name}
-        WHERE CardID = ? AND LearnDate IS NULL
-        ORDER BY NextReviewDate ASC
-        LIMIT 1
-    '''
-    result = fetch_all(query, (card_id,))
-
-    if result:
-        next_review_date = result[0][0]
-        query = "UPDATE flashcards_database SET NextReviewDate = ? WHERE id = ?"
-        execute_query(query, (next_review_date, card_id))
+    elif study_count - review_count > 0:
+        return 1
     else:
-        query = "UPDATE flashcards_database SET status = 3 WHERE id = ?"
-        execute_query(query, (card_id,))
+        return 2
 
 
-def record_flashcard_count(choice, flashcard):
-    if choice in ('yes', 'no'):
-        record_count(choice, flashcard)
-    else:
-        print("Invalid choice")
+def insert_review_plan(user_id, card_id, study_count, review_count):
+    study_status = calculate_study_status(study_count, review_count)
+    next_review_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+    cursor.execute(
+        "INSERT INTO ReviewPlans (UserID, CardID, StudyStatus, ReviewCount, NextReviewDate) VALUES (?, ?, ?, ?, ?)",
+        (user_id, card_id, study_status, review_count, next_review_date))
+    conn.commit()
 
 
-def should_update_flashcard(flashcard):
-    query = f"SELECT YesCount, NoCount FROM {LEARNING_TABLE_NAME} WHERE CardID = ?"
-    result = fetch_all(query, (flashcard.id,))
-    yes_count, no_count = result[0] if result else (0, 0)
-
-    if yes_count > 0 or no_count > 0:
-        return True
-    else:
-        return False
+def get_next_review_date_and_status(user_id, card_id):
+    cursor.execute("SELECT NextReviewDate, StudyStatus FROM ReviewPlans WHERE UserID = ? AND CardID = ?",
+                   (user_id, card_id))
+    review_plan = cursor.fetchone()
+    if review_plan:
+        return review_plan[0], review_plan[1]
+    return None, None
 
 
-def cleanup_and_exit(flashcards):
-    for flashcard in flashcards:
-        if should_update_flashcard(flashcard):
-            status = decide_learning_status(flashcard)
-            query = f"""
-                UPDATE flashcards_database
-                SET status = ?, LearnDate = (SELECT LearnDate FROM {LEARNING_TABLE_NAME} WHERE CardID = ?)
-                WHERE id = ?
-            """
-            execute_query(query, (status, flashcard.id, flashcard.id))
-    create_individual_learning_tables()
-    record_learning_status_for_individual_tables()
+def get_study_date(user_id, card_id):
+    cursor.execute("SELECT StudyDate FROM StudyRecords WHERE UserID = ? AND CardID = ?",
+                   (user_id, card_id))
+    study_record = cursor.fetchone()
+    if study_record:
+        return study_record[0]
+    return None
 
-    query = f"DELETE FROM {LEARNING_TABLE_NAME} WHERE LearningStatus = 2 AND (YesCount > 0 OR NoCount > 0)"
-    execute_query(query)
-    conn.close()
-    print("学习完成！")
+
+def insert_or_update_user_word_card(user_id, card_id):
+    next_review_date, study_status = get_next_review_date_and_status(user_id, card_id)
+    study_date = get_study_date(user_id, card_id)
+
+    if next_review_date is not None:
+        cursor.execute(
+            "INSERT OR REPLACE INTO UserWordCard (UserID, CardID, StudyDate, NextReviewDate, StudyStatus) VALUES (?, ?, ?, ?, ?)",
+            (user_id, card_id, study_date, next_review_date, study_status))
+        conn.commit()
 
 
 def main():
-    initialize_tables()
+    username = input("请输入用户名: ")
+    user_exists = check_user_existence(username)
+
+    if not user_exists:
+        password = input("请输入密码: ")
+        registration_date = datetime.now().strftime("%Y-%m-%d")
+        insert_user(username, password, registration_date)
+        print("用户已注册!")
+
     flashcards = get_flashcards()
+    user_id = cursor.execute("SELECT UserID FROM Users WHERE Username = ?", (username,)).fetchone()[0]
     quit_program = False
 
     while flashcards and not quit_program:
         flashcard = random.choice(flashcards)
         show_flashcard(flashcard)
-        choice = input("\nChoose an action (yes/no) or press 'q' to quit: ").lower()
+        choice = input("\nChoose an action (1/2/3) or press 'q' to quit: ")
 
-        if choice == 'q':
+        if choice == '1':
+            insert_study_record(user_id, flashcard.id, 1, 0)
+        elif choice == '2':
+            insert_study_record(user_id, flashcard.id, 0, 1)
+        elif choice == '3':
+            # 如果用户选择 '3'，标记卡片状态为 '已学会' 并从学习卡片列表中移除
+            cursor.execute(
+                "INSERT INTO ReviewPlans (UserID, CardID, StudyStatus) VALUES (?, ?, ?)",
+                (user_id, flashcard.id, 3))
+            conn.commit()
+
+            flashcards.remove(flashcard)
+
+        elif choice == 'q':
             quit_program = True
-        else:
-            record_flashcard_count(choice, flashcard)
 
-    cleanup_and_exit(flashcards)
+    # 在退出时，为每个卡片创建复习计划
+    for flashcard in flashcards:
+        study_record = cursor.execute(
+            "SELECT StudyCount, ReviewCount FROM StudyRecords WHERE UserID = ? AND CardID = ?",
+            (user_id, flashcard.id)).fetchone()
+        if study_record:
+            study_count, review_count = study_record
+            insert_review_plan(user_id, flashcard.id, study_count, review_count)
+            insert_or_update_user_word_card(user_id, flashcard.id)
+
+    conn.commit()
 
 
 if __name__ == "__main__":
--- translate.py
+++ translate.py
@@ -5,15 +5,12 @@ conn = sqlite3.connect('flashcards_database.db')
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS flashcards_database (
-        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-        Status INTEGER,
+    CREATE TABLE IF NOT EXISTS Cards (
+        CardId INTEGER PRIMARY KEY AUTOINCREMENT,
         Phonetic TEXT,
         Word TEXT NOT NULL,
         Sentence TEXT,
-        Audio_url TEXT,
-        LearnDate DATE,
-        NextReviewDate DATE
+        Audio_url TEXT
     )
 ''')
 # 因为音标符号有点特殊，所以需要加上编码方式，不然有些会出现乱码
@@ -22,9 +19,49 @@ with open('data/url_file.csv', 'r', encoding="utf-8") as csvfile:
     next(csvreader)  # 跳过CSV文件的标题行（如果有）
     for row in csvreader:
         # 不包括 id 列
-        cursor.execute("INSERT INTO flashcards_database (Status, Phonetic, Word, Sentence, Audio_url, LearnDate, "
-                       "NextReviewDate) "
-                       "VALUES (?, ?, ?, ?, ?, ?, ?)", row)
+        cursor.execute("INSERT INTO Cards (Phonetic, Word, Sentence, Audio_url )VALUES (?, ?, ?, ?)", row)
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Username VARCHAR NOT NULL,
+        Password VARCHAR,
+        RegistrationDate DATE
+    )
+''')
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS UserWordCard (
+    RecordID INTEGER PRIMARY KEY,
+    UserID INTEGER,
+    CardID INTEGER,
+    StudyDate DATE,
+    NextReviewDate DATE,
+    StudyStatus INTEGER
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS StudyRecords (
+    RecordID INTEGER PRIMARY KEY,
+    UserID INTEGER,
+    CardID INTEGER,
+    StudyDate DATE,
+    StudyCount INTEGER,
+    ReviewCount INTEGER
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ReviewPlans (
+    PlanID INTEGER PRIMARY KEY,
+    UserID INTEGER,
+    CardID INTEGER,
+    StudyStatus INTEGER,
+    ReviewCount INTEGER,
+    NextReviewDate DATE
+)
+""")
 
 # 提交所有的插入操作
 conn.commit()
